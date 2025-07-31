4f113c710d3ce9529391a6bc5c2dee70f73ca256
mweinstein7 <maxw86343@gmail.com>
2025-07-30 21:24:46 -0700

    Add day 1 and day 2 Python filesDay 1 and 2 Python Files

---
+++ day_2/variables.py
@@ -0,0 +1,51 @@
+## Day 2: 30 Days of Python Programming
+
+# Exercises: Level 1
+first_name = "Max"
+last_name = "Weinstein"
+full_name = first_name + last_name 
+country = "USA"
+city = "Sacramento"
+age = 18
+year = 2025
+is_married = False
+is_true = True
+is_light_on = True 
+first_name, last_name, country = "Max", "Weinstein", "USA"
+
+## Exercises: Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+diff = num_one - num_two
+product = num_one / num_two
+division = num_one / num_two
+remainder = num_two % num_one 
+variable_exp = num_one ** num_two
+floor_division = num_one // num_two
+
+radius = 30
+area_of_circle = 3.14 * (radius ** 2)
+circum_of_circle = 2 * 3.14 * radius
+
+radius = float(input("What is the radius?: "))
+area_of_circle = 3.14 * (radius ** 2)
+print("The area is:", area_of_circle)
+
+first_name = input("What is your first name?: ")
+last_name = input("What is your last name?: ")
+country = input("What country do you live in?: ")
+age = int(input("How old are you?: "))
