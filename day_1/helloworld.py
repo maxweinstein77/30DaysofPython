b3622422bc7c2f158f557a1d1bae9d198d5e1efd
mweinstein7 <maxw86343@gmail.com>
2025-09-02 15:04:57 -0500

    Day 1/30 - 30DaysofPython

---
+++ day_1/helloworld.py
@@ -0,0 +1,57 @@
+### Day 1
+
+## Level 2
+
+# 1
+# python3 --version
+
+# 2
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+# 3
+print("Max")
+print("Weinstein")
+print("USA")
+print("I am enjoying 30 Days of Python")
+
+# 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(["Max", "Python", "USA"]))
+print("Max")
+print("Weinstein")
+print("USA")
+
+## Level 3
+
+# 1
+print(3) # Integer
+print(3.1) # Float
+print(3 - 3j) # Complex
+print("howdy!") # String
+print(True) # Boolean
+print(["Apple", "Banana", "Orange"]) # List
+print((1, 2, 3)) # Set
+print({
+    "name": "Max",
+    "language": "Python",
+    "country": "USA"
+}) # Dictionary
+
+# 2
+import math
+
+x_1 = 2
+x_2 = 10
+y_1 = 3
+y_2 = 8
+
+math.sqrt(((x_2 - x_1)**2) + (y_2 - y_1)**2) 
