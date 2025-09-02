bf62d94548a5a8b5903b6b5239a291f8b8fc95e3
mweinstein7 <maxw86343@gmail.com>
2025-09-02 16:21:39 -0500

    Create variables.py

---
+++ day_2/variables.py
@@ -0,0 +1,88 @@
+### Day 2
+
+## Level 1
+
+# 2
+# Day 2: 30 Days of Python Programming
+
+# 3
+first_name = "Max"
+last_name = "Weinstein"
+full_name = first_name + last_name
+country = "USA"
+city = "Austin"
+age = 18
+year = "2025"
+is_married = False
+is_true = True 
+is_light_on = True 
+is_short, is_fast, is_happy = False, True, True 
+
+## Level 2
+
+# 1
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(is_short))
+print(type(is_fast))
+print(type(is_happy))
+
+
+# 2
+print(len(first_name))
+
+# 3
+print(len(last_name)) # Last name is longer than first name
+
+# 4
+num_one = 5
+num_two = 4
+
+# 5
+total = num_one + num_two
+
+# 6
+diff = num_one - num_two
+
+# 7
+product = num_one * num_two
+
+# 8
+division = num_one / num_two
+
+# 9
+remainder = num_one % num_two
+
+# 10
+exp = num_one ** num_two
+
+# 11
+floor_division = num_one // num_two
+
+# 12
+radius = 30
+
+area_of_circle = 3.14 * radius ** 2
+
+circum_of_circle = 2 * 3.14 * radius 
+
+radius = float(input("Radius: "))
+area_of_circle = 3.14 * radius ** 2
+print(area_of_circle)
+
+# 13
+first_name = input("First name: ")
+last_name = input("Last name: ")
+country = input("Country: ")
+age = input("Age: ")
+
+# 14
+help("keywords")
